92f2b8d6d826f7d93af3d28409e27079892ab772
akshatva7 <135726741+akshatva7@users.noreply.github.com>
2023-10-17 23:39:46 +0530

    Update checksum_tb.v

--- checksum_tb.v
+++ checksum_tb.v
@@ -1,15 +1,40 @@
 `include "checksum_generate.v"
-module _8bitchecksum_tb();
+module bitchecksum_tb();
     reg [31:0] data;
+    reg clk;
     wire [7:0] checksum;
-    
-    _8bitchecksum dut(.data(data),.checksum(checksum));
+          
+    bitchecksum dut(.data(data),.checksum(checksum),.clk(clk));
+           
+   
         initial begin
-            data=32'h00000001;
-            #10
-            $display("%b",checksum);
-
+            clk=0;
+            forever
             
+            #10 clk=~clk;
+            end
+            initial begin 
+            #8;
+            data=32'h00000001;
+            #10;
+            data=32'h00000010;
+            #10;
+            data=32'h00000100;
+            #10;
+            data=32'h00001000;
+            #10;
+            data=32'h00010000;
+            #10;
+            data=32'h00100000;
+            #10;
+            data=32'h01000000;
+            #10;
+            data=32'h10000000;
+            #10;
+                        
         end
+        initial begin
+        $dumpfile("checksum.vcd");
+        $dumpvars(1,bitchecksum_tb);
+    end
 endmodule
-
